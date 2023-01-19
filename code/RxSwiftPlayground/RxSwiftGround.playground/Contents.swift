937628ee1a1d613174d48884c57cc854ec84ab81
BJCHO0501 <bjcho1503@naver.com>
2023-01-19 22:47:13 +0900

    playground

--- code/RxSwiftPlayground/RxSwiftGround.playground/Contents.swift
+++ code/RxSwiftPlayground/RxSwiftGround.playground/Contents.swift
@@ -1,47 +1,221 @@
 import UIKit
 import RxSwift
+import RxCocoa
 
 // MARK: - just, of, from
+//
+//let one = 1
+//let two = 2
+//let three = 3
+//
+//let observable1: Observable<Int> = Observable<Int>.just(one)
+//
+////타입 추론함
+//let observable2 = Observable.of([one, two, three])
+//
+////배열 요소를 하나씩 반환
+//let observable3 = Observable.from([one, two, three])
+//
+//// MARK: - subscribe(onNext:_)
+//
+//observable1
+//    .subscribe(onNext: {
+//    print($0)
+//})
+//
+//observable2
+//    .subscribe(onNext: {
+//    print($0)
+//})
+//
+//observable3
+//    .subscribe(onNext: {
+//    print($0)
+//})
+//
+//// MARK: - empty()
+//
+//let emptyObservable = Observable<Void>.empty()
+//
+//emptyObservable.subscribe(
+//    onNext: {
+//        print($0)
+//    },
+//    onCompleted: {
+//        print("Completed")
+//    }
+//)
+//
+//// MARK: - .never()
+//
+//let neverObservable = Observable<Any>.never()
+//
+//neverObservable.subscribe(
+//    onNext: {
+//        print($0)
+//    },
+//
+//    onCompleted: {
+//        print("Completed")
+//    }
+//)
+//
+//// MARK: - .range()
+//
+//let rangeObservable = Observable<Int>.range(start: 1, count: 10)
+//
+//rangeObservable.subscribe(
+//    onNext: {
+//        print("range: \($0)")
+//    },
+//    onCompleted: {
+//        print("Completed")
+//    }
+//)
 
-let one = 1
-let two = 2
-let three = 3
+// MARK: - Disposing
 
-let observable1: Observable<Int> = Observable<Int>.just(one)
+//let disposeBag = DisposeBag()
+//
+//let disposeObservable = Observable.of(4)
+//
+//disposeObservable.subscribe(
+//    onNext: {
+//        print($0)
+//    },
+//    onDisposed: {
+//        print("Disposed")
+//    }
+//).disposed(by: disposeBag)
 
-//타입 추론함
-let observable2 = Observable.of([one, two, three])
+// MARK: - PublishSubjects
 
-//배열 요소를 하나씩 반환
-let observable3 = Observable.from([one, two, three])
+//print("=============PublishSubjects=============\n")
 
-// MARK: - subscribe(onNext:_)
+//let subject = PublishSubject<String>()
+//     subject.onNext("Is anyone listening?")
+//
+//     let subscriptionOne = subject
+//         .subscribe(onNext: { (string) in
+//             print(string)
+//         })
+//     subject.on(.next("1"))
+//     subject.onNext("2")
+//
+//     // 1
+//     let subscriptionTwo = subject
+//         .subscribe({ (event) in
+//             print("2)", event.element ?? event)
+//         })
+//
+//     // 2
+//     subject.onNext("3")
+//
+//     // 3
+//     subscriptionOne.dispose()
+//     subject.onNext("4")
+//
+//     // 4
+//     subject.onCompleted()
+//
+//     // 5
+//     subject.onNext("5")
+//
+//     // 6
+//     subscriptionTwo.dispose()
+//
+//     let disposeBag = DisposeBag()
+//
+//     // 7
+//     subject
+//         .subscribe {
+//             print("3)", $0.element ?? $0)
+//     }
+//         .disposed(by: disposeBag)
+//
+//     subject.onNext("?")
 
-observable1
-    .subscribe(onNext: {
-    print($0)
-})
+// MARK: - BehaviorSubjects
 
-observable2
-    .subscribe(onNext: {
-    print($0)
-})
+//print("=============BehaviorSubjects=============\n")
 
-observable3
-    .subscribe(onNext: {
-    print($0)
-})
+enum MyError: Error {
+    case anError
+}
 
-// MARK: - empty()
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    print(label, event.element ?? event.error ?? event as Any)
+}
+//
+//let subject = BehaviorSubject(value: "Initial value")
+//let disposeBag = DisposeBag()
+//
+//subject.onNext("X")
+//
+//subject
+//   .subscribe{
+//       print(label: "1)", event: $0)
+//   }
+//   .disposed(by: disposeBag)
+//
+//subject.onError(MyError.anError)
+//
+//subject
+//   .subscribe {
+//       print(label: "2)", event: $0)
+//   }
+//   .disposed(by: disposeBag)
 
-let emptyObservable = Observable<Void>.empty()
+// MARK: - ReplaySubjects
 
-emptyObservable.subscribe(
-    onNext: {
-        print($0)
-    },
-    onCompleted: {
-        print("Completed")
+//print("=============ReplaySubjects=============\n")
+//
+//let subject = ReplaySubject<String>.create(bufferSize: 2)
+//let disposeBag = DisposeBag()
+//
+//
+//subject.onNext("1")
+//subject.onNext("2")
+//subject.onNext("3")
+//
+//subject
+// .subscribe {
+//     print(label: "1)", event: $0)
+// }
+// .disposed(by: disposeBag)
+//
+//subject
+// .subscribe {
+//     print(label: "2)", event: $0)
+// }
+// .disposed(by: disposeBag)
+//
+//subject.onNext("4")
+//subject.onError(MyError.anError)
+//subject.dispose()
+//
+//subject
+//    .subscribe {
+//        print(label: "3)", event: $0)
+//    }
+//    .disposed(by: disposeBag)
+
+// MARK: - Variables
+
+print("=============Variables=============\n")
+
+let variable = BehaviorRelay(value: "first")
+let disposeBag = DisposeBag()
+
+var brhavior = variable.value
+
+
+variable.asObservable()
+    .subscribe {
+        print(label: "1)", event: $0)
     }
-)
+    .disposed(by: disposeBag)
+
+brhavior.append("asdfa")
+variable.accept(brhavior)
 
